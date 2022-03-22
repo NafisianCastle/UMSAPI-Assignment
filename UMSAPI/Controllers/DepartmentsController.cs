c445b3e0419fb6fd95ebd1c7caed9d6933c64973
NafisianCastle <nafis.cseaiub@gmail.com>
2022-03-22 23:24:48 +0600

    Add project files.

---
+++ UMSAPI/Controllers/DepartmentsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using UMSAPI.Models.Database;
+using EntityState = System.Data.Entity.EntityState;
+
+namespace UMSAPI.Controllers
+{
+    public class DepartmentsController : ApiController
+    {
+        private readonly UMSEntities _db = new UMSEntities();
+
+        // GET: api/Departments
+        public IQueryable<Department> GetDepartments()
+        {
+            return _db.Departments;
+        }
+
+        // GET: api/Departments/5
+        [ResponseType(typeof(Department))]
+        public IHttpActionResult GetDepartment(int id)
+        {
+            var department = _db.Departments.Find(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(department);
+        }
+
+        // PUT: api/Departments/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDepartment(int id, Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != department.Id)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(department).State = EntityState.Modified;
+
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Departments
+        [ResponseType(typeof(Department))]
+        public IHttpActionResult PostDepartment(Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _db.Departments.Add(department);
+            _db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = department.Id }, department);
+        }
+
+        // DELETE: api/Departments/5
+        [ResponseType(typeof(Department))]
+        public IHttpActionResult DeleteDepartment(int id)
+        {
+            var department = _db.Departments.Find(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            _db.Departments.Remove(department);
+            _db.SaveChanges();
+
+            return Ok(department);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DepartmentExists(int id)
+        {
+            return _db.Departments.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ UMSAPI/Controllers/StudentsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using UMSAPI.Models.Database;
+using EntityState = System.Data.Entity.EntityState;
+
+namespace UMSAPI.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        private readonly UMSEntities _db = new UMSEntities();
+
+        // GET: api/Students
+        public IQueryable<Student> GetStudents()
+        {
+            return _db.Students;
+        }
+
+        // GET: api/Students/5
+        [ResponseType(typeof(Student))]
+        public IHttpActionResult GetStudent(int id)
+        {
+            var student = _db.Students.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+        // PUT: api/Students/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutStudent(int id, Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != student.Id)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                _db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Students
+        [ResponseType(typeof(Student))]
+        public IHttpActionResult PostStudent(Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _db.Students.Add(student);
+            _db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = student.Id }, student);
+        }
+
+        // DELETE: api/Students/5
+        [ResponseType(typeof(Student))]
+        public IHttpActionResult DeleteStudent(int id)
+        {
+            var student = _db.Students.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _db.Students.Remove(student);
+            _db.SaveChanges();
+
+            return Ok(student);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _db.Students.Count(e => e.Id == id) > 0;
+        }
+    }
+}
